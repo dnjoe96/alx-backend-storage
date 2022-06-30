c99e302d7a196c9b446f92417a2113fa66ed95ca
dnjoe96 <donjoedbest@gmail.com>
2022-06-30 13:48:33 -0700

    Add Auto increment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- Create a table with unique field
 
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
+	name VARCHAR(255),
+	PRIMARY KEY (id)
 	);
