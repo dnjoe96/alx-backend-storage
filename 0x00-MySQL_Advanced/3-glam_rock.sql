47297ec7d4332960570dc8d90e7f4ee402562c84
dnjoe96 <donjoedbest@gmail.com>
2022-07-02 13:29:52 -0700

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 
 SELECT band_name,
 CASE WHEN split is NULL THEN
-	(YEAR(CURDATE()) - 2 - formed) ELSE 
+	(YEAR(CURDATE()) - formed) ELSE 
 	(split - formed) END AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style)
