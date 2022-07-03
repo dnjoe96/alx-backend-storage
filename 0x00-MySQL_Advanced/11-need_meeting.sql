f4102bcdaf035dc1f665a125a62f97701c502a29
dnjoe96 <donjoedbest@gmail.com>
2022-07-03 00:02:26 -0700

    Modify columns selected

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,7 +1,7 @@
 -- Create view need_meeting
 
 CREATE VIEW need_meeting AS
-SELECT * FROM students
+SELECT name FROM students
 WHERE score < 80
 AND (last_meeting is NULL 
 OR last_meeting < DATE_SUB(NOW(), interval 1 month));
