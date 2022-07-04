70a6600a8c0b816d3a68a7be7b4f88827af28c4c
dnjoe96 <donjoedbest@gmail.com>
2022-07-04 13:06:18 +0100

    Command python codes

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+""" Module for update_topics function """
+
 
 def update_topics(mongo_collection, name, topics):
+    """ The function updates a document in a collection with
+    a list of topics
+    """
     myquery = { "name": name }
     newvalues = { "$set": { "topics": topics } }
 
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+""" Module for schools_by_topic function """
+
 
 def schools_by_topic(mongo_collection, topic):
+    """ The function returns all schools that have a certain topic
+    in their list of topics
+    """
     all_schools = mongo_collection.find({"topics": topic})
     return all_schools
