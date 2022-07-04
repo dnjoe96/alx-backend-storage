c84d0c26d19c4b7ac6ac935a67d640e092f3e8d5
dnjoe96 <donjoedbest@gmail.com>
2022-07-04 16:33:16 +0100

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@ def update_topics(mongo_collection, name, topics):
     """ The function updates a document in a collection with
     a list of topics
     """
-    myquery = { "name": name }
-    newvalues = { "$set": { "topics": topics } }
+    myquery = {"nae": name}
+    newvalues = {"$set": {"topics": topics}}
 
     mongo_collection.update_one(myquery, newvalues)
