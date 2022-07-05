2a3097b430feff41bb0896ab07bd299c1a209d17
dnjoe96 <donjoedbest@gmail.com>
2022-07-05 14:20:18 -0700

    102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -13,6 +13,14 @@ if __name__ == '__main__':
     patch = the_logs.count_documents({"method": "PATCH"})
     delete = the_logs.count_documents({"method": "DELETE"})
     status = the_logs.count_documents({"path": "/status"})
+    ip_list = [
+            "172.31.63.67", "172.31.2.14", "172.31.29.194",
+            "69.162.124.230", "64.124.26.109", "64.62.224.29",
+            "34.207.121.61", "47.88.100.4", "45.249.84.250",
+            "216.244.66.228"
+            ]
+    ip_dict = {ip: the_logs.count_documents({"ip": ip}) for ip in ip_list}
+
     print(f"{total} logs")
     print(f"Methods:")
     print(f"\tmethod GET: {get}")
@@ -22,12 +30,15 @@ if __name__ == '__main__':
     print(f"\tmethod DELETE: {delete}")
     print(f"{status} status check")
     print("IPs:")
-    ip_list = [
-            "172.31.63.67", "172.31.2.14", "172.31.29.194",
-            "69.162.124.230", "64.124.26.109", "64.62.224.29",
-            "34.207.121.61", "47.88.100.4", "45.249.84.250",
-            "216.244.66.228"
-            ]
-    for ip in ip_list:
-        val = the_logs.count_documents({"ip": ip})
-        print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
+    print(f"\t{ip}: {val}")
