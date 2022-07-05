64e9127c0ab986045d53542f6d95c211cc9091b0
dnjoe96 <donjoedbest@gmail.com>
2022-07-05 13:57:31 -0700

    Include IP

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Module for top_students function """
+
+def find_average(score_list):
+    total_score = 0
+    count = 0
+    
+    for one in score_list:
+        # print(one.get('score'))
+        total_score += one['score']
+        count += 1
+    if total_score == 0:
+        return 0
+    return total_score / count
+
+def top_students(mongo_collection):
+    """ The function returns all documents in a collection"""
+    students = mongo_collection.find()
+    top = []
+    for student in students:
+        student['averageScore'] = find_average(student.get('topics'))
+        student.pop('topics')
+        top.append(student)
+    return top
---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Script to gather log summary from nginx logs """
+
+if __name__ == '__main__':
+    from pymongo import MongoClient
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    the_logs = client.logs.nginx
+
+    total = the_logs.count_documents({})
+    get = the_logs.count_documents({"method": "GET"})
+    post = the_logs.count_documents({"method": "POST"})
+    put = the_logs.count_documents({"method": "PUT"})
+    patch = the_logs.count_documents({"method": "PATCH"})
+    delete = the_logs.count_documents({"method": "DELETE"})
+    status = the_logs.count_documents({"path": "/status"})
+    print(f"{total} logs")
+    print(f"Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{status} status check")
+    print("IPs:")
+    ip_list = [
+            "172.31.63.67", "172.31.2.14", "172.31.29.194", 
+            "69.162.124.230", "64.124.26.109", "64.62.224.29", 
+            "34.207.121.61", "47.88.100.4", "45.249.84.250",
+            "216.244.66.228"
+            ]
+    for ip in ip_list:
+        print('\t{}: {}'.format(ip, the_logs.count_documents({"ip": ip})))
