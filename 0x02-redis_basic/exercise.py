353882023f86031cd3c2ae78914670e92e65e39f
dnjoe96 <donjoedbest@gmail.com>
2022-07-09 11:53:03 +0100

    Create decorator function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 """Module defines `Cache` class"""
 import redis
-from typing import Union, Optional, Callable
+from typing import Union, Optional, Callable, Any
 from uuid import uuid4
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    """ record the number of time method is called
+    Args:
+        method (Callable): The decorated function
+    Returns:
+        Wrapper function
+    """
+    @functools.wraps(method)
+    def count_calls_wrapper(self, *args: Any, **kwargs: Any) -> Any:
+        """ Wrapper method for provided wrapped method """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+
+    return count_calls_wrapper
 
 
 class Cache:
@@ -14,6 +31,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[bytes, float, int, str]) -> str:
         """A function that store data in a unique key
         Args:
