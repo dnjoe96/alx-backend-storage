688722cb9ead7cdfb1bce5a6f054bc76752f8609
dnjoe96 <donjoedbest@gmail.com>
2022-07-09 11:44:12 +0100

    Add get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module defines `Cache` class"""
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 
 
@@ -24,3 +24,25 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> DB_VAL_TYPS:
+        """ Method to get value at `key`
+        Args:
+            key (str): The key
+            fn (Callable): Function that decodes to utf-8
+        Returns:
+            data from server
+        """
+        raw_data = self._redis.get(key)
+        if fn is not None:
+            return fn(raw_data)
+        else:
+            return raw_data
+
+    def get_str(self, data: bytes) -> str:
+        """ Method returns string type-casted data """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Method returns integer type-casted data """
+        return int(data)
